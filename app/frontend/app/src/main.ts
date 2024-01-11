7902ea0b19fbe2a59f8d6a59d802a344f0e9050a
Samuel To <samuelto888@yahoo.co.uk>
2024-01-11 16:37:55 +0000

    add bootstrap

--- app/frontend/app/src/main.ts
+++ app/frontend/app/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 createApp(App).use(router).mount("#app");
