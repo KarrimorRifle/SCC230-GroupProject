e245fb9113c772342f9851456e6b1b43121cd213
Samuel To <samuelto888@yahoo.co.uk>
2024-01-11 18:05:09 +0000

    clean main

--- app/frontend/app/src/main.ts
+++ app/frontend/app/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
-import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.min.css";
