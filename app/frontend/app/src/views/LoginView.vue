6078e19d47a2db8a3e098efccd3249aaf29fa9ba
Samuel To <samuelto888@yahoo.co.uk>
2024-01-11 19:23:10 +0000

    create frontend of login page

--- app/frontend/app/src/views/LoginView.vue
+++ app/frontend/app/src/views/LoginView.vue
@@ -1,26 +1,61 @@
 <template>
-  <ul class="dropdown-menu dropdown-menu-end me-3">
-    <form action="" v-if="!loggedIn" class="p-3 pb-0">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-      <div class="form-text">Don't have an account? <a>Sign Up</a></div>
-    </form>
-    <div v-else></div>
-  </ul>
+  <div
+    class="d-flex align-items-start justify-content-center"
+    style="height: 90vh"
+    data-bs-theme="dark"
+  >
+    <div
+      class="card"
+      style="width: 35%; margin-top: 6rem; height: 23rem; padding-top: 1rem"
+    >
+      <form action="" class="p-3 pb-0 card-body">
+        <div class="mb-3">
+          <label
+            for="exampleInputEmail1"
+            class="form-label text-start ps-2"
+            style="width: 100%"
+            ><b>Email address</b></label
+          >
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="mb-3">
+          <label
+            for="exampleInputPassword1"
+            class="form-label text-start ps-2"
+            style="width: 100%"
+            ><b>Password</b></label
+          >
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+          <div class="form-text" style="width: 100%">
+            <a class="link">Forgot password</a>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary mb-5">Submit</button>
+        <div class="form-text">
+          Don't have an account? <a class="link">Sign Up</a>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+const login = () => {
+  console.log("logging in...");
+};
+</script>
+
+<style>
+a.link {
+  text-decoration: underline !important;
+  cursor: pointer;
+}
+</style>
