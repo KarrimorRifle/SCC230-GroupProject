864566467de7ca6947f95342a36045a258023d8e
Samuel To <samuelto888@yahoo.co.uk>
2024-01-17 19:11:12 +0000

    run lint and minor bug fix

--- app/frontend/app/src/components/AccountDropdown.vue
+++ app/frontend/app/src/components/AccountDropdown.vue
@@ -29,10 +29,10 @@ let User = {
   Surname: "Jone",
 };
 </script>
-<script>
-import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'AccountDropdown'
-})
+  name: "AccountDropdown",
+});
 </script>
