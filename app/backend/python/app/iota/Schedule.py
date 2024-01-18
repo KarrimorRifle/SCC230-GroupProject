48004378ca398d8d04641942df5f8eac8c1e77d0
Kain Tompson <76596634+TheBathToaster@users.noreply.github.com>
2024-01-18 17:32:13 +0000

    Add framework for code translation
    Add comments to the functionCode Class

--- app/backend/python/app/iota/Schedule.py
+++ app/backend/python/app/iota/Schedule.py
@@ -12,12 +12,21 @@ from iota.Device import Device
 
 ##CLASS DEFINITIONS##
 class functionCode():
-    def __init__(self, commandType:str, commandNo:int, linkedCommands:str = None, parameters:[]=None):
+    ##VALUES##
+    #commandType    The type of command the code is, for translating to python i.e. IF, FOR, ELSE etc.
+    #number         Which command of commandType the code is
+    #name           The name of the command, to be used throughout the code
+    #linkedCommands The commands it is linked to, i.e. which if statement an else statement is linked to
+    #parameters     The parameters that the command takes
+    #hasRun         Whether or not the command has run or not - useful for if/else statments
+
+    def __init__(self, commandType:str, number:int, linkedCommands:[str]=None, parameters:[]=None):
         self.commandType = commandType
-        self.commandID = commandNo
-        self.commandName = commandType+str(commandNo)
+        self.number = number
+        self.name = commandType+str(number)
         self.linkedCommands = linkedCommands
         self.parameters = parameters
+        self.hasRun = False
     
 class Schedule:
     ##VALUES##
@@ -46,19 +55,33 @@ class Schedule:
         self.isActive = isActive
         self.code = code
 
+
+
 #Translates the schedule to actual code
-def translateSchedule(code):
-    for i in range(code[0].length):
-        match((code[0][i][0]).upper):
+def translateSchedule(code:[functionCode]):
+    for i in range(code.length):
+        match(code[i].commandType):
             case "FOR":
-                print()
+                for code[i].commandName in range (code[i].paramaters[0]):
+                    pass
+                    #Functionality to find end of loop and recursively execute code in loop.
             case "WHILE":
-                print()
+                while(eval(f"{code[i].paramaters[0]}{code[i].paramaters[1]}{code[i].paramaters[2]}")):
+                      pass
+                    #Functionality to find end of loop and recursively execute code in loop.
             case "IF":
-               print()
+                if(eval(f"{code[i].paramaters[0]}{code[i].paramaters[1]}{code[i].paramaters[2]}")):
+                    pass
+                    #Functionality to find end of if statement and recursively execute code in loop.
             case "OTHERWISE":
-                print()
-            case "END":
-                print()
+                if(code[i].paramaters.length == 0):
+                    pass
+                    #Find related functionCode and check if it has run
+                else:
+                    pass
+                    #Find related functionCode and check if it has run then add parameters for elif statement
             case "SET":
-                print()
+                pass
+                #set a value to param 2 (requires prereq devices working)
+            case _:
+                pass
