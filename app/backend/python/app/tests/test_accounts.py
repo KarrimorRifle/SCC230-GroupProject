61ce88b1cc6edaf3bf934f8c1377b23b5b294d7d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-01-20 02:39:06 +0000

    Create test_accounts.py

---
+++ app/backend/python/app/tests/test_accounts.py
@@ -0,0 +1,24 @@
+import unittest
+from server import app
+
+from flask import request, jsonify, make_response
+
+class test_accounts(unittest.TestCase):
+    def setUp(self):
+        self.client_server = app.test_client()
+
+    def test_accounts_creation_valid(self):
+        response = self.client_server.post('/accounts', json={"FirstName": "test", "Surname": "one", "Email" : "testone@test.com", "Password" : "pass1"})
+    
+        self.assertEqual(response.status_code, 200)
+
+    def test_accounts_get_account(self):
+        self.client_server.post('/accounts', json={"FirstName": "test", "Surname": "zero", "Email" : "testzero@test.com", "Password" : "pass0"})
+
+        self.assertIsNotNone(self.client_server.get('/accounts/getAccount'))
+
+    def test_accounts_creation_used_email(self):
+        self.client_server.post('/accounts', json={"FirstName": "test", "Surname": "two", "Email" : "testwo@test.com", "Password" : "pass2"})
+        response = self.client_server.post('/accounts', json={"FirstName": "test", "Surname": "two", "Email" : "testwo@test.com", "Password" : "pass2"})
+    
+        self.assertEqual(response.status_code, 409)
