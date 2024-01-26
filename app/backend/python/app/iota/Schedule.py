e26ba2ea4e46e8501b3354736b439b16b411efda
Kian Tomkins <K.D.Tomkins@outlook.com>
2024-01-26 19:25:50 +0000

    Schedule.py Structure Changes
    Move functions into the class.
    Make supporting functions private.
    Add framework for connecting schedule to devices.
    Add framework for GET functionality.

--- app/backend/python/app/iota/Schedule.py
+++ app/backend/python/app/iota/Schedule.py
@@ -2,7 +2,7 @@
 #Desc:          File to hold the Schedule Class and related Functions
 #               The Function of the Schedule Class is to run the user-defined code once a trigger is activated
 #
-#Last Update:   2024-1-18
+#Last Update:   2024-1-26
 #Updated By:    Kian Tomkins
 #Interpreter:   Python 3.11
 
@@ -11,22 +11,23 @@ from iota.Trigger import Trigger
 from iota.Device import Device
 
 ##CLASS DEFINITIONS##
-class functionCode():
+class FunctionCode():
     ##VALUES##
-    #commandType    The type of command the code is, for translating to python index.e. IF, FOR, ELSE etc.
+    #commandType    The type of command the code is, for translating to python i.e. IF, FOR, ELSE etc.
     #number         Which command of commandType the code is
     #name           The name of the command, to be used throughout the code
-    #linkedCommands The commands it is linked to, index.e. which if statement an else statement is linked to
+    #linkedCommands The commands it is linked to, i.e. which if statement an else statement is linked to
     #params         The params that the command takes
-    #hasRun         Whether or not the command has run or not - useful for if/else statments
+    #hasRun         Holds the number of times a FunctionCode has run
 
-    def __init__(self, commandType:str, number:int, linkedCommands:[str]=[], params:list=[]):
+    ##CONSTRUCTOR##
+    def __init__(self, commandType:str, number:int, linkedCommands:list=[], params:list=[]):
         self.commandType = commandType
         self.number = number
         self.name = commandType+str(number)
         self.linkedCommands = linkedCommands
         self.params = params
-        self.hasRun = False
+        self.hasRun = 0
     
 class Schedule:
     ##VALUES##
@@ -38,95 +39,109 @@ class Schedule:
     #devices        Holds the devices linked to the schedule
     #isRunning      Checks if the schedule is currently running
     #isActive       Checks if the schedule is currently able to run
-    #functionCode   Holds the untranslated code for a schedule
+    #FunctionCode   Holds the untranslated code for a schedule
 
     ##CONSTRUCTOR##
-    def __init__(self, id:str, name:str, rating:int=0, isPublic:bool=False,
-                 triggers:[Trigger]=[], devices:[Device]=[], code:[functionCode] = [],
-                 isRunning:bool=False, isActive:bool=False):
+    def __init__(self, id:str, name:str, isPublic:bool=False, rating:int=1,
+                 triggers:[Trigger]=[], code:[FunctionCode] = [], isActive:bool=False):
         self.id = id
         self.name = name
         self.isPublic = isPublic
         if(isPublic):
             self.rating = rating
         self.triggers = triggers
-        self.devices = devices
-        self.isRunning = isRunning
         self.isActive = isActive
         self.code = code
+        self.devices = self.findDevices()
 
+    ##PUBLIC METHODS##
+    #Translates the schedule to actual code
+    def translateSchedule(self, index:int=0) -> int:
+        #Checks the type of statement that is at code[index]
+        match(self.code[index].commandType):
+            #Code for a for loop
+            case "FOR":
+                for i in range (self.code[index].params[0]):
+                    self.code[index].hasRun+=1
+                    self.__runConditional(self.code, index)
 
-#Finds the end of a conditional statement or loop
-def findEnd(code:[functionCode], index:int, statement:functionCode):
-    while(code[index].commandType!="END" and not (statement.name in code[index].linkedCommands)):
-        index+=1
-    return index
+                #Returns the location of the end of the for loop, where the code should jump to next.
+                return(self.__findEnd(self.code, index, self.code[index]))
+            #Code for a while loop
+            case "WHILE":
+                while(eval(f"{' '.join(self.code[index].params)}")):
+                    self.code[index].hasRun+=1
+                    self.__runConditional(self.code, index)
 
-#Runs all the lines in a condition statement or loop
-def runConditional(code:[functionCode], index:int):
-    #Creates a temporary index to run the required commands
-    _index=index
-    #Loops through every statement until the end statement
-    while(_index != findEnd(code,index,code[index])):
-        _index+=1
-        translateSchedule(code=code, index=_index)
+                #Returns the location of the end of the for loop, where the code should jump to next.
+                return(self.__findEnd(self.code, index, self.code[index]))
+            #Code for an if statement
+            case "IF":
+                if(eval(f"{' '.join(self.code[index].params)}")):
+                    self.code[index].hasRun+=1
+                    self.__runConditional(self.code, index)
+                    
+                #Returns the location of the end of the for loop, where the code should jump to next.
+                return(self.__findEnd(self.code, index, self.code[index]))
+            #Code for an else/else if statement
+            case "OTHERWISE":
+                #Value to check if all conditions for the else statement to be run are met
+                allConditions=True
+
+                #Checks if the if statement is correct, if the OTHERWISE statement is equal to an elif statement
+                if(len(self.code[index].params) != 0):
+                    if(not(eval(f"{' '.join(self.code[index].params)}"))):
+                        allConditions=False
+                #Checks if any of the linked if and elif statements ran
+                for i in range (len(self.code[index].linkedCommands)):
+                    if(self.code[index].linkedCommands[i].hasRun>0):
+                        allConditions=False
+                    break
+                #Runs the code if all the checks pass
+                if(allConditions):
+                    self. code[index].hasRun+=1
+                    self.__runConditional(self.code, index)
+
+                #Returns the location of the end of the for loop, where the code should jump to next.
+                return(self.__findEnd(self.code, index, self.code[index]))
+            #Code for a set statement
+            case "SET":
+                #set a value of param 1 to param 2 (requires prereq devices working)
+                self.code[index].hasRun+=1
+                return index+1
+            case "GET":
+                #get value of param 1 (requires prereq devices working)
+                self.code[index].hasRun+=1
+                return index+1
+            #Default case to ignore End or invalid statements
+            case _:
+                pass
+
+    def findDevices(self) -> [Device]:
+        #Find all the devices that the schedule uses. 
+        return []
+
+    def initDevices(self) -> {Device: bool}:
+        #Checks if all the devices connected to the schedule can be accessed
+        #Calls the functions to initiate connections. 
+        pass
         
+    ##PRIVATE METHODS##
+    #Runs all the lines in a condition statement or loop
+    def __runConditional(self, index:int):
+        #Creates a temporary index to run the required commands
+        _index=index
+        #Loops through every statement until the end statement
+        while(_index != self.__findEnd(self.code, index, self.code[index])):
+            _index+=1
+            self.translateSchedule(code=self.code, index=_index)
 
-#Translates the schedule to actual code
-def translateSchedule(code:[functionCode], index:int=0):
-    #Checks the type of statement that is at code[index]
-    match(code[index].commandType):
-        #Code for a for loop
-        case "FOR":
-            for i in range (code[index].params[0]):
-                code[index].hasRun=True
-                runConditional(code, index)
+    #Finds the end of a conditional statement or loop
+    def __findEnd(self, index:int, statement:FunctionCode) -> int:
+        while(self.code[index].commandType!="END" and not (statement.name in self.code[index].linkedCommands)):
+            index+=1
+        return index
 
-            #Returns the location of the end of the for loop, where the code should jump to next.
-            return(findEnd(code,index,code[index]))
-        #Code for a while loop
-        case "WHILE":
-            while(eval(f"{' '.join(code[index].params)}")):
-                code[index].hasRun=True
-                runConditional(code, index)
-
-            #Returns the location of the end of the for loop, where the code should jump to next.
-            return(findEnd(code,index,code[index]))
-        #Code for an if statement
-        case "IF":
-            if(eval(f"{' '.join(code[index].params)}")):
-                code[index].hasRun=True
-                runConditional(code, index)
-                
-            #Returns the location of the end of the for loop, where the code should jump to next.
-            return(findEnd(code,index,code[index]))
-        #Code for an else/else if statement
-        case "OTHERWISE":
-            #Value to check if all conditions for the else statement to be run are met
-            allConditions=True
-
-            #Checks if the if statement is correct, if the OTHERWISE statement is equal to an elif statement
-            if(len(code[index].params) != 0):
-                if(not(eval(f"{' '.join(code[index].params)}"))):
-                    allConditions=False
-            #Checks if any of the linked if and elif statements ran
-            for i in range (len(code[index].linkedCommands)):
-                if(code[index].linkedCommands[i].hasRun):
-                   allConditions=False
-                   break
-            
-            if(allConditions):
-                code[index].hasRun=True
-                runConditional(code, index)
-
-            #Returns the location of the end of the for loop, where the code should jump to next.
-            return(findEnd(code,index,code[index]))
-        #Code for a set statement
-        case "SET":
-            print(code[index].params[0])
-            code[index].hasRun=True
-            return index+1
-            #set a value to param 2 (requires prereq devices working)
-        #Default case to ignore End or invalid statements
-        case _:
-            pass
+    def __warnUser(self):
+        #Email the user to let them know a schedule failed, with the reasons behind it.
+        pass
--- app/backend/python/app/iota/User.py
+++ app/backend/python/app/iota/User.py
@@ -17,15 +17,17 @@ class User:
     #username       Holds the username that the user will see
     #password       Holds the password which the user will need to access their account
     #email          Holds the email tied to the user's account
+    #allowEmails    Whether or not the the user would like to recieve Emails. 
     #houseRoles     Holds the user's homes and their permission level within them (User:int)
     #schedules      Holds the schedules the user has saved
 
     ##CONSTRUCTOR##
-    def __init__(self, id:str, username:str, password:str, email:str="",
-                 houseRoles:dict={}, schedules:Schedule=[]):
+    def __init__(self, id:str, username:str, password:str, email:str,
+                 allowEmails:bool, houseRoles:dict={}, schedules:Schedule=[]):
         self.id = id
         self.username=username
         self.password=password
         self.email=email
         self.houseRoles = houseRoles
         self.schedules = schedules
+        self.allowEmails = allowEmails
