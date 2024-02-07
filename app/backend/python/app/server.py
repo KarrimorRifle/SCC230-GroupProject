096dce517d999e97f9fbf60dd7822e61fe23a216
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-07 18:47:04 +0000

    Update server.py
    add user blueprint

--- app/backend/python/app/server.py
+++ app/backend/python/app/server.py
@@ -1,10 +1,7 @@
 import mysql.connector
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-import bcrypt
-import uuid
-from datetime import datetime, timedelta
-from routes.accounts import accounts
+from iota.User import user
 
 #db connection
 connection = mysql.connector.connect(
@@ -18,67 +15,8 @@ cursor = connection.cursor(dictionary = True)
 app = Flask(__name__)
 app.config['cursor'] = cursor
 app.config['connection'] = connection
-app.register_blueprint(accounts)
+app.register_blueprint(user)
 CORS(app, supports_credentials=True)
-
-#handling paths and different methods
-#@app.route("/path/<param>", methods = ['requestMethod1','requestMethod2'])
-#def home(param):
-#   if request.method == 'requestMethod1'
-#    return "Home!"
-#   else
-#       return jsonify(object), statusCode
-##getting query params
-# var = request.args.get("queryParamName")
-@app.route("/login", methods=["POST"])
-def login():
-    email = request.json.get("Email")
-    password = request.json.get("Password")
-    password = password.encode("utf-8")
-    
-    query = f"SELECT * FROM accounts WHERE Email = %s"
-    cursor.execute(query, (email,))
-    account = cursor.fetchone()
-    if account is None:
-        return jsonify({"error":"Details don't match our system"}), 403
-
-    if bcrypt.checkpw(password, account.get("Password").encode("utf-8")):
-        response = make_response(jsonify({"success":"Logged in successfully"}))
-        sessionID = str(uuid.uuid4())
-        sessionExp = datetime.now() + timedelta(days = 1)
-
-        try:
-            cursor.execute("UPDATE accounts SET SessionID = %s, SessionExp = %s WHERE AccountID = %s",(sessionID,sessionExp,account.get("AccountID")))
-            connection.commit()
-        except Exception as e:
-            connection.rollback()
-            return jsonify({'error':"Internal server error", "details":f"{e}"}), 500
-        
-        response.set_cookie('session_id', sessionID, max_age=24*60*60)
-        return(response)
-
-    else:
-        return jsonify({"error":"Details don't match our system"}), 403
-    
-@app.route("/logout", methods=["DELETE"])
-def logout():
-    sessionID = request.cookies.get('session_id')
-    try:
-        cursor.execute("UPDATE accounts SET SessionID = NULL, SessionExp = %s WHERE SessionID = %s",(datetime.now(), sessionID))
-        connection.commit()
-    except Exception as e:
-        connection.rollback()
-        return jsonify({'error':"Internal server error", "details":f"{e}"}), 500
-    return
-
-def getAccount():
-    sessionID = request.cookies.get('session_id')
-    query = ("SELECT * FROM accounts "
-                "WHERE SessionID = %s")
-    
-    cursor.execute(query, (sessionID))
-    return cursor.fetchone()
-        
 
 #running app
 if __name__ == "__main__":
