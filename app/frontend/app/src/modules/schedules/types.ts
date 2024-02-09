f31cbbc2b7d98efca732f94db02fba4b43a58949
Samuel To <samuelto888@yahoo.co.uk>
2024-02-09 12:59:37 +0000

    create required types for schedules

--- app/frontend/app/src/modules/schedules/types.ts
+++ app/frontend/app/src/modules/schedules/types.ts
@@ -1,6 +1,32 @@
 
 type CommandType = 'FOR' | 'WHILE' | "IF" | "OTHERWISE" | "SET" | "GET" | "END";
 
+//functions of either of these types aren't required on front end hence unimplemented
 interface FunctionCode{
-  commandType: 
+  commandType: CommandType,
+  number: number,
+  linkedCommands?: FunctionCode[],
+  params?: string[]
+}
+
+interface Schedule{
+  id: string,
+  name: string,
+  isPublic: boolean,
+  isActive: boolean,
+  ratings: number[],//was just one value, but instead should be a computed value
+  triggers: Trigger[],
+  code: FunctionCode[],
+}
+
+interface Trigger{
+  id: string,
+  data: Record<string, string[]>, //deviceID: data
+}
+
+interface Device{
+  id: string,
+  name: string,
+  isActive: boolean,
+  data: Record<string,any>,
 }
