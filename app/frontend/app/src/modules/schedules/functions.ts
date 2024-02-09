590a6805e659bf6f8d06bf8935f737aaee4db9f8
Samuel To <samuelto888@yahoo.co.uk>
2024-02-09 15:37:54 +0000

    add stuff to the actual schedule items itself

--- app/frontend/app/src/modules/schedules/functions.ts
+++ app/frontend/app/src/modules/schedules/functions.ts
@@ -1,5 +1,54 @@
 import { Schedule } from "./types";
 export const getSchedules = async (): Promise<Schedule[]> => {
   //use user details to return schedules
-  return [<Schedule>{}, <Schedule>{}, <Schedule>{}, <Schedule>{}];
+  return [
+    <Schedule>{
+      id: "01",
+      name: "name1",
+      isPublic: false,
+      isActive: false,
+      ratings: [],
+      triggers: [],
+      code: [],
+    },
+    <Schedule>{
+      id: "02",
+      name: "name2",
+      isPublic: false,
+      isActive: false,
+      ratings: [],
+      triggers: [],
+      code: [],
+    },
+    <Schedule>{
+      id: "03",
+      name: "name3",
+      isPublic: false,
+      isActive: false,
+      ratings: [],
+      triggers: [],
+      code: [],
+    },
+    <Schedule>{
+      id: "04",
+      name: "name4",
+      isPublic: false,
+      isActive: false,
+      ratings: [],
+      triggers: [],
+      code: [],
+    },
+  ];
+};
+
+export const getScheduleByID = async (id: string): Promise<Schedule> => {
+  return <Schedule>{
+    id: "01",
+    name: "name1",
+    isPublic: false,
+    isActive: false,
+    ratings: [],
+    triggers: [],
+    code: [],
+  };
 };
--- app/frontend/app/src/views/schedules/ScheduleList.vue
+++ app/frontend/app/src/views/schedules/ScheduleList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-container d-flex">
     <div class="main row bg-dark">
-      <div class="col-2 options bg-secondary"></div>
+      <div class="col-2 options bg-gray"></div>
       <div class="col-10 schedules px-0">
         <h2 class="text-start text-light underlined bg-dark px-3 py-2">
           Schedules
@@ -13,13 +13,45 @@
               class="form-control filter"
               placeholder="Search for schedule"
             />
-            <button class="input-group-text bg-secondary">SEARCH</button>
+            <button class="input-group-text bg-gray">SEARCH</button>
           </div>
           <button class="btn btn-primary">CREATE</button>
         </div>
         <div class="px-3">
-          <div v-for="item in schedules" :key="item.id" class="card mb-3">
-            <div class="card-body">{{ item.name }}hi</div>
+          <div
+            v-for="item in schedules"
+            :key="item.id"
+            class="card mb-3 bg-gray"
+          >
+            <div
+              class="card-title mb-0 text-start p-2 d-flex justify-content-between"
+            >
+              <h5 class="me-2">{{ item.name }}</h5>
+              <div class="d-flex">
+                <a
+                  :href="'/schedules/' + item.id"
+                  class="btn btn-info me-2 btn-sm"
+                  >EDIT</a
+                >
+                <div
+                  class="p-2"
+                  :class="{
+                    'bg-success': item.isActive,
+                    'bg-danger': !item.isActive,
+                  }"
+                  style="
+                    width: 1rem;
+                    height: 1rem;
+                    border-radius: 50%;
+                    border-width: 1px;
+                    border-color: black;
+                    border-style: solid;
+                  "
+                />
+              </div>
+            </div>
+            <hr class="m-0" />
+            <div class="card-body py-0">data</div>
           </div>
         </div>
       </div>
@@ -50,5 +82,9 @@ fetchData();
 .main {
   height: 100vh;
   width: 90%;
+}
+
+.bg-gray {
+  background-color: rgb(35, 39, 49);
 }
 </style>
