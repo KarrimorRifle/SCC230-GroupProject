5ffbd57df5bfee0d25e58e827c27d61ac810c681
Samuel To <samuelto888@yahoo.co.uk>
2024-02-09 13:30:27 +0000

    create dummy function for schedules

---
+++ app/frontend/app/src/modules/schedules/functions.ts
@@ -0,0 +1,5 @@
+import {Schedule} from "./types"
+export const retrieveSchedules = async(): Promise<Schedule[]> => {
+  //use user details to return schedules
+  return [<Schedule>{},<Schedule>{},<Schedule>{},<Schedule>{},]
+}
