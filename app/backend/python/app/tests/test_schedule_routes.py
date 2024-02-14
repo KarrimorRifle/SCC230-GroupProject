ed86518a486a48ca76eaeae2147808f310851701
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-14 14:49:37 +0000

    Update test_schedule_routes.py
    add tests for delete schedule

--- app/backend/python/app/tests/test_schedule_routes.py
+++ app/backend/python/app/tests/test_schedule_routes.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from server import app
 
 from flask import request, jsonify, make_response
@@ -7,25 +8,35 @@ class TestScheduleRoutes(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
         self.client_server.post("/login", json={"Email": "jhondoe@gmail.com", "Password": "JhonDoe123."})
-        self.client_server.post("/schedule", json={'Name': 'Test Schedule1'})
 
-#     def test_create_schedule_success(self):
-#         code_payload = {}
-#         response = self.client_server.post('/schedule', json={'Name':'Schedule1', 'Code': code_payload})
-#         self.assertEqual(response, 200)
+    def test_create_schedule_success(self):
+        response = self.client_server.post("/schedule", json={'ScheduleName': 'Test Schedule1', 'IsActive': 0, 'IsPublic': 0, 'Rating': 0})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('EventID', response.data.decode('utf-8'))
 
-    def test_get_schedules_success(self):
-        response = self.client_server.get('/schedule')
-        print(response.data.decode('utf-8'))
+    def test_delete_schedule_success(self):
+        response = self.client_server.post("/schedule", json={'ScheduleName': 'Test Schedule2', 'IsActive': 0, 'IsPublic': 0, 'Rating': 0})
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        url = '/schedule/'
+        url += str(data['EventID'])
+        response = self.client_server.delete(url)
         self.assertEqual(response.status_code, 200)
 
-#     def test_get_schedule_details_success(self):
-#         response = self.client_server.get('/schedule/details', json={'ScheduleID':'1'})
-#         self.assertEqual(response, 200)
-#         self.assertIn('Code', response.data)
-#         self.assertIn('Variables', response.data)
-#         self.assertIn('Devices', response.data)
-    
-#     def test_delete_schedule_success(self):
-#         response = self.client_server.delete('/schedule/details', json={'ScheduleID':'1'})
-#         self.assertEqual(response, 200)
+    def test_get_schedules_success(self):
+        response = self.client_server.post("/schedule", json={'ScheduleName': 'Test Schedule2', 'IsActive': 0, 'IsPublic': 0, 'Rating': 0})
+        self.assertEqual(response.status_code, 200)
+        response = self.client_server.get('/schedule')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        for entry in data:
+            self.assertIn('EventID', entry)
+            self.assertIn('ScheduleName', entry)
+            self.assertIn('IsActive', entry)
+            self.assertIn('IsPublic', entry)
+            self.assertIn('Rating', entry)
+
+    # def test_get_schedule_details_success(self):
+    #     response = self.client_server.get('/schedule/1')
+    #     print(response.data.decode('utf-8'))
+    #     self.assertEqual(response.status_code, 200)
