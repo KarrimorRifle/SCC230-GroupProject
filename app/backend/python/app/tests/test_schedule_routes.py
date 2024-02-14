179a934983791e44fa7e0b19b6a11eb88bad2b8f
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-14 15:08:47 +0000

    Update test_schedule_routes.py
    create test for get specific schedule

--- app/backend/python/app/tests/test_schedule_routes.py
+++ app/backend/python/app/tests/test_schedule_routes.py
@@ -36,7 +36,21 @@ class TestScheduleRoutes(unittest.TestCase):
             self.assertIn('IsPublic', entry)
             self.assertIn('Rating', entry)
 
-    # def test_get_schedule_details_success(self):
-    #     response = self.client_server.get('/schedule/1')
-    #     print(response.data.decode('utf-8'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_get_schedule_details_success(self):
+        response = self.client_server.post("/schedule", json={'ScheduleName': 'Test Schedule2', 'IsActive': 0, 'IsPublic': 0, 'Rating': 0})
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        url = '/schedule/'
+        url += str(data['EventID'])
+        response = self.client_server.get(url)
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('EventID', data)
+        self.assertIn('ScheduleName', data)
+        self.assertIn('IsActive', data)
+        self.assertIn('IsPublic', data)
+        self.assertIn('Rating', data)
+        self.assertIn('AuthorID', data)
+        self.assertIn('Code', data)
+        self.assertIn('Triggers', data)
