13b0309e4de17a4d958557f0617d826c68477228
MrNudy <blade@eastie.net>
2024-02-14 22:30:46 +0000

    removed placeholder test to prevent errors.

--- app/backend/python/app/tests/test_Iota_class.py
+++ app/backend/python/app/tests/test_Iota_class.py
@@ -7,7 +7,7 @@ class test_User(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
 
-    def test_Iota(self):
+    #def test_Iota(self):
         #response = self.client_server.post('/User', json={"FirstName": "test", "Surname": "one", "Email" : "testone@test.com", "Password" : "pass1"})
     
         #self.assertEqual(response.status_code, 200)
