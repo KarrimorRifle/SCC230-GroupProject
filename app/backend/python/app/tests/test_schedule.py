980b24accc34a066e07029889d309d6d17d8ce32
MrNudy <blade@eastie.net>
2024-02-18 13:45:49 +0000

    second test added

--- app/backend/python/app/tests/test_schedule.py
+++ app/backend/python/app/tests/test_schedule.py
@@ -9,7 +9,12 @@ class test_accounts(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
 
-    def test_schedule_creation(self):
-        test = Schedule.Schedule("Sched1","testSched",True,3,[],[],False,False)
+    def test_schedule_creation_max_args(self):
+        test = Schedule.Schedule("Sched1","testSched",True,3,[],False,False)
 
         self.assertEqual(test['rating'], 3)
+
+    def test_schedule_creation_min_args(self):
+        test = Schedule.Schedule("Sched1","testSched")
+
+        self.assertEqual(test['name'], "testSched")
