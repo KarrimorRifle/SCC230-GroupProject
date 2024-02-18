682b86a56a1cedd44315bfc54b0b4cec458a25bb
MrNudy <blade@eastie.net>
2024-02-18 13:37:03 +0000

    trial3

--- app/backend/python/app/tests/test_schedule.py
+++ app/backend/python/app/tests/test_schedule.py
@@ -10,13 +10,6 @@ class test_accounts(unittest.TestCase):
         self.client_server = app.test_client()
 
     def test_schedule_creation(self):
-        test = iota.Schedule("Sched1","testSched",True,3,[],[],False,False)
-        self.assertEqual(test['rating'], 3)
+        test = schedule("Sched1","testSched",True,3,[],[],False,False)
 
-    
-    # def test_accounts_get_returns_correct(self):
-    #     response = self.client_server.post("/login", json={"Email": "jhondoe@gmail.com", "Password": "JhonDoe123."})
-    #     self.assertEqual(response.status_code, 200)
-        
-    # response = self.client_server.get("/accounts")
-    # self.assertEqual(loads(response.text)['AccountID'], "Accojk42VvlqdeBpOBc")
+        self.assertEqual(test['rating'], 3)
