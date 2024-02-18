04737a08de4fa046054c0404cbd84882591192e0
MrNudy <blade@eastie.net>
2024-02-18 13:41:31 +0000

    trial5

--- app/backend/python/app/tests/test_schedule.py
+++ app/backend/python/app/tests/test_schedule.py
@@ -3,13 +3,13 @@ from server import app
 import sys
 from json import loads
 sys.path.append((sys.path[0])[:-6])
-import iota.Schedule as schedule
+import iota.Schedule as Schedule
 
 class test_accounts(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
 
     def test_schedule_creation(self):
-        test = schedule.schedule("Sched1","testSched",True,3,[],[],False,False)
+        test = Schedule.Schedule("Sched1","testSched",True,3,[],[],False,False)
 
         self.assertEqual(test['rating'], 3)
