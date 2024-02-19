047022d675224864ccbad0237fd1d140ece3e7a3
Samuel To <samuelto888@yahoo.co.uk>
2024-02-19 14:35:39 +0000

    Add schedule editor and block editor

---
+++ app/frontend/app/src/views/schedules/BlockEditor.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="editor" class="main">item</div>
+</template>
+<script setup lang="ts"></script>
+<style>
+#editor {
+  background-color: rgb(81, 90, 113);
+  height: 100%;
+}
+</style>
---
+++ app/frontend/app/src/views/schedules/ScheduleEdit.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main mx-0">
+    <div class="header">
+      <h2>Name of the schedule</h2>
+    </div>
+    <div class="container-fluid mx-0 px-0">
+      <div class="row mx-0">
+        <div class="col-9 px-0">hello</div>
+        <div class="col-3 px-0">
+          <block-editor />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import BlockEditor from "./BlockEditor.vue";
+</script>
+<style>
+.main {
+  height: 100vh;
+  width: 100%;
+  background-color: rgb(35, 39, 49);
+}
+
+.header {
+  background-color: rgb(58, 64, 81);
+  min-height: 4rem;
+}
+
+.container-fluid {
+  height: 100%;
+}
+
+.row {
+  height: 100%;
+}
+
+body {
+  color: white;
+}
+</style>
