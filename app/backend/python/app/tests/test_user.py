b0e5c54ff4fbe5c49c5821d5a53fb4e1cb10a955
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-20 21:59:34 +0000

    Update test_user.py

--- app/backend/python/app/tests/test_user.py
+++ app/backend/python/app/tests/test_user.py
@@ -1,14 +1,21 @@
 import unittest
 from server import app
 import iota.User as User
+import sys
+sys.path.append((sys.path[0])[:-6])
 
 class test_user(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
 
     def test_user_loadfromdatabase(self):
-        test = User.loadFromDatabase("TestTrigger")
+        test = User.loadFromDatabase("Accojk42VvlqdeBpOBc")
 
-        expected = User.User("TestTrigger",{'TestDevice':''},"TestSchedule")
+        expected = User.User("Accojk42VvlqdeBpOBc", "Jhon", None, "jhondoe@gmail.com")
 
-        self.assertEqual(test,expected)
+        self.assertEqual(test.id,expected.id)
+        self.assertEqual(test.username,expected.username)
+        self.assertEqual(test.password,expected.password)
+        self.assertEqual(test.email,expected.email)
+        self.assertEqual(test.allowEmails,expected.allowEmails)
+        self.assertEqual(test.debug,expected.debug)
