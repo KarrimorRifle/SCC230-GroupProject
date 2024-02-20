0fa0fc1053e3fd9b99df26f15878a18131de76a3
MrNudy <blade@eastie.net>
2024-02-20 16:35:42 +0000

    again

--- app/backend/python/app/iota/Trigger.py
+++ app/backend/python/app/iota/Trigger.py
@@ -41,7 +41,7 @@ def loadFromDatabase(id:str):
         nextLine = cursor.fetchone()
         data = {}
         while(nextLine != None):
-            data[nextLine['DeviceID']][nextLine['PosNum']] = nextLine['data']
+            data[nextLine['DeviceID']][nextLine['PosNum']] = nextLine['Data']
             nextLine = cursor.fetchone()
         return Trigger(str(Trigger['TriggerID']), data, ScheduleID)
     else:
