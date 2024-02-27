62b636acb1f462466ca44222aa893e860f61670d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-27 00:37:47 +0000

    Update test_hub_routes.py

--- app/backend/python/app/tests/test_hub_routes.py
+++ app/backend/python/app/tests/test_hub_routes.py
@@ -18,11 +18,13 @@ class TestHubRoutes(unittest.TestCase):
         data = json.loads(response.data)
         url = '/hub/'
         url += data['HubID']
+
         response = self.client_server.delete(url)
         self.assertEqual(response.status_code, 200)
+        self.assertIn('HubID', response.data.decode('utf-8'))
 
     def test_get_hubs_success(self):
-        response = self.client_server.post("/hub", json={'HubName': 'Test Hub1'})
+        response = self.client_server.post("/hub", json={'HubName': 'Test Hub'})
         self.assertEqual(response.status_code, 200)
         self.assertIn('HubID', response.data.decode('utf-8'))
 
@@ -36,7 +38,7 @@ class TestHubRoutes(unittest.TestCase):
             self.assertIn('HubName', entry)
     
     def test_get_one_hub_success(self):
-        response = self.client_server.post("/hub", json={'HubName': 'Test Hub1'})
+        response = self.client_server.post("/hub", json={'HubName': 'Test Hub3'})
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         url = '/hub/'
@@ -44,8 +46,10 @@ class TestHubRoutes(unittest.TestCase):
         response = self.client_server.get(url)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
+
         self.assertIn('HubID', data)
         self.assertIn('HubName', data)
+        self.assertEqual(data['HubName'], 'Test Hub3')
 
     def test_update_hub_success(self):
         response = self.client_server.post("/hub", json={'HubName': 'Test Hub1'})
