46d27b360e51837c767e9fc7e3c2c4fafcad8f3c
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-29 21:53:21 +0000

    Add remove user from hub functionality

--- app/backend/python/app/routes/hub/user/hub_user.py
+++ app/backend/python/app/routes/hub/user/hub_user.py
@@ -4,10 +4,10 @@ from iota import genRandomID
 
 hub_user = Blueprint('hub_user', __name__)
 
-def get_hub_users(account, cursor, HubID):
+def get_hub_users(account, cursor, hubID):
 
     query = ("SELECT HubID FROM accounts_hubsRelation WHERE HubID = %s AND AccountID = %s")
-    cursor.execute(query, (HubID, account['AccountID']))
+    cursor.execute(query, (hubID, account['AccountID']))
     hub = cursor.fetchone()
     if not hub:
         return jsonify({'error': 'User not part of hub'}), 401
@@ -16,7 +16,7 @@ def get_hub_users(account, cursor, HubID):
     query = ("SELECT accounts.*, accounts_hubsRelation.PermissionLevel FROM accounts_hubsRelation "
              "JOIN accounts ON accounts_hubsRelation.AccountID = accounts.AccountID "
              "WHERE HubID = %s")
-    cursor.execute(query, (HubID,))
+    cursor.execute(query, (hubID,))
     users = cursor.fetchall()
 
     if not users:
@@ -27,3 +27,22 @@ def get_hub_users(account, cursor, HubID):
         userList.append({'AccountID': user['AccountID'], 'Name': user['FirstName'] + " " + user['LastName'], 'PermissionLevel': user['PermissionLevel']})
     userList = sorted(userList, key=lambda x: (x['PermissionLevel'], x['Name']))
     return jsonify(userList), 200
+
+def remove_hub_user(account, cursor, hubID, accountID):
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE HubID = %s AND AccountID = %s")
+    cursor.execute(query, (hubID, account['AccountID']))
+    hub = cursor.fetchone()
+    if not hub:
+        return jsonify({'error': 'User not part of hub'}), 401
+
+    if hub['PermissionLevel'] < 4:
+        return jsonify({'error': 'User does not have permission to remove users from hub'}), 403
+
+    query = ("DELETE FROM accounts_hubsRelation WHERE HubID = %s AND AccountID = %s")
+    try:
+        cursor.execute(query, (hubID, accountID))
+        current_app.config['connection'].commit()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    return jsonify({'HubID': hubID, 'AccountID': accountID}), 200
