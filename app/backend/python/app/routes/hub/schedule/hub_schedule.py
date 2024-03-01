ef578aecc36f441311894087e289d0d39b4e2895
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-03-01 14:14:37 +0000

    Create get schedules in hub functionality

--- app/backend/python/app/routes/hub/schedule/hub_schedule.py
+++ app/backend/python/app/routes/hub/schedule/hub_schedule.py
@@ -3,3 +3,20 @@ from ...accounts import getAccount
 from iota import genRandomID
 
 hub_schedule = Blueprint('hub_schedule', __name__)
+
+def get_hub_schedules(account, cursor, hubID):
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    cursor.execute(query, (account['AccountID'], hubID))
+    permissionLevel = cursor.fetchone()['PermissionLevel']
+    if not permissionLevel:
+        return jsonify({'error': 'User not in Hub'}), 401
+    
+    if permissionLevel == 0:
+        return jsonify({'error': 'User does not have permission to view schedules'}), 403
+    
+    query = ("SELECT ScheduleID, ScheduleName, IsActive, accounts.FirstName, accounts.Surname, PermissionLevel FROM schedules JOIN accounts ON schedules.AuthorID = accounts.AccountID JOIN accounts_hubsRelations ON schedules.AuthorID = accounts_hubsRelations.AccountID WHERE schedules.HubID = %s")
+    cursor.execute(query, (hubID,))
+    schedules = cursor.fetchall()
+    schedules = [{'ScheduleID': schedule['ScheduleID'], 'ScheduleName': schedule['ScheduleName'], 'IsActive': schedule['IsActive'], 'Author': schedule['FirstName']+" "+schedule['Surname'], 'PermissionLevel': schedule['PermissionLevel']} for schedule in schedules]
+    schedules = sorted(schedules, x=lambda x: (x['PermissionLevel'], x['Author'], x['ScheduleName']))
+    return jsonify(schedules), 200
