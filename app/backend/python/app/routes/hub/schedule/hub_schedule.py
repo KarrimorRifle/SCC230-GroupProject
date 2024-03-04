588d503d7a315493f51431eb3da384c490af9908
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-03-04 22:33:39 +0000

    Change hubs list return order

--- app/backend/python/app/routes/hub/schedule/hub_schedule.py
+++ app/backend/python/app/routes/hub/schedule/hub_schedule.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify, Blueprint, current_app
+import json
 from ...accounts import getAccount
 from iota import genRandomID
 from ...schedule.schedule import get_schedule_detail, delete_schedule, create_schedule, update_schedule
@@ -6,27 +7,28 @@ from ...schedule.schedule import get_schedule_detail, delete_schedule, create_sc
 hub_schedule = Blueprint('hub_schedule', __name__)
 
 def get_hub_schedules(account, cursor, hubID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
-    permissionLevel = cursor.fetchone()['PermissionLevel']
+    permissionLevel = cursor.fetchone()
     if not permissionLevel:
         return jsonify({'error': 'User not in Hub'}), 401
+    permissionLevel = permissionLevel['PermissionLevel']
     
     if permissionLevel == 0:
         return jsonify({'error': 'User does not have permission to view schedules'}), 403
     
     query = ("SELECT ScheduleID, ScheduleName, IsActive, IsDraft, accounts.FirstName, accounts.Surname, PermissionLevel FROM schedules "
              "JOIN accounts ON schedules.AuthorID = accounts.AccountID "
-             "JOIN accounts_hubsRelations ON schedules.AuthorID = accounts_hubsRelations.AccountID "
+             "JOIN accounts_hubsRelation ON schedules.AuthorID = accounts_hubsRelation.AccountID "
              "WHERE schedules.HubID = %s")
     cursor.execute(query, (hubID,))
     schedules = cursor.fetchall()
     schedules = [{'ScheduleID': schedule['ScheduleID'], 'ScheduleName': schedule['ScheduleName'], 'IsActive': schedule['IsActive'], 'IsDraft': schedule['IsDraft'], 'Author': schedule['FirstName']+" "+schedule['Surname'], 'PermissionLevel': schedule['PermissionLevel']} for schedule in schedules]
-    schedules = sorted(schedules, x=lambda x: (x['PermissionLevel'], x['Author'], x['ScheduleName']))
+    schedules = sorted(schedules, key=lambda x: (x['PermissionLevel'], x['Author'], x['ScheduleName']))
     return jsonify(schedules), 200
 
 def get_one_hub_schedule(account, cursor, hubID, scheduleID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
     permissionLevel = cursor.fetchone()['PermissionLevel']
     if not permissionLevel:
@@ -35,15 +37,15 @@ def get_one_hub_schedule(account, cursor, hubID, scheduleID):
     if permissionLevel == 0:
         return jsonify({'error': 'User does not have permission to view schedules'}), 403
     
-    schedule_details = get_schedule_detail(account, cursor, scheduleID, True)
+    schedule_details = json.loads(get_schedule_detail(account, cursor, scheduleID, True)[0].data)
 
-    if schedule_details['HubID'] != hubID:
+    if schedule_details.get('HubID') != hubID:
         return jsonify({'error': 'Schedule not in Hub'}), 403
     
     return jsonify(schedule_details), 200
 
 def add_hub_schedule(account, cursor, connection, hubID, scheduleID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
     permissionLevel = cursor.fetchone()['PermissionLevel']
     if not permissionLevel:
@@ -52,18 +54,19 @@ def add_hub_schedule(account, cursor, connection, hubID, scheduleID):
     if permissionLevel < 3:
         return jsonify({'error': 'User does not have permission to add schedules'}), 403
     
-    schedule = get_schedule_detail(account, cursor, scheduleID, True)
+    schedule = json.loads(get_schedule_detail(account, cursor, scheduleID, True)[0].data)
     if schedule.get('error') is not None:
         return jsonify({'error': 'Schedule not found'}), 404
 
-    newID = create_schedule(account, cursor, connection, schedule).get('ScheduleID')
+    newID = json.loads(create_schedule(account, cursor, connection, schedule)[0].data).get('ScheduleID')
     schedule['Trigger'] = None
+
     query = ("UPDATE schedules SET HubID = %s WHERE ScheduleID = %s")
     cursor.execute(query, (hubID, newID))
     return update_schedule(account, cursor, connection, newID, schedule, True)
 
 def remove_hub_schedule(account, cursor, connection, hubID, scheduleID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
     permissionLevel = cursor.fetchone()['PermissionLevel']
     if not permissionLevel:
@@ -80,7 +83,7 @@ def remove_hub_schedule(account, cursor, connection, hubID, scheduleID):
     return delete_schedule(account, cursor, connection, scheduleID, True)
 
 def update_hub_schedule(account, cursor, connection, hubID, scheduleID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
     permissionLevel = cursor.fetchone()['PermissionLevel']
     if not permissionLevel:
@@ -97,7 +100,7 @@ def update_hub_schedule(account, cursor, connection, hubID, scheduleID):
     return update_schedule(account, cursor, connection, scheduleID, request.json, True)
 
 def create_hub_schedule(account, cursor, connection, hubID):
-    query = ("SELECT PermissionLevel FROM accounts_hubsRelations WHERE AccountID = %s AND HubID = %s")
+    query = ("SELECT PermissionLevel FROM accounts_hubsRelation WHERE AccountID = %s AND HubID = %s")
     cursor.execute(query, (account['AccountID'], hubID))
     permissionLevel = cursor.fetchone()['PermissionLevel']
     if not permissionLevel:
@@ -106,14 +109,14 @@ def create_hub_schedule(account, cursor, connection, hubID):
     if permissionLevel < 3:
         return jsonify({'error': 'User does not have permission to create schedules'}), 403
     
-    newID = create_schedule(account, cursor, connection, request.json, True).get('ScheduleID')
+    newID = json.loads(create_schedule(account, cursor, connection, request.json)[0].data).get('ScheduleID')
 
     query = ("UPDATE schedules SET HubID = %s WHERE ScheduleID = %s")
     cursor.execute(query, (hubID, newID))
 
-    return jsonify({'ScheduleID': newID}), 200
+    return get_schedule_detail(account, cursor, newID, True)
 
-@hub_schedule.route('/hub/<hubID>/schedule', methods=['GET'])
+@hub_schedule.route('/hub/<hubID>/schedule', methods=['GET', 'POST'])
 def hub_schedule_routes(hubID):
     account = getAccount()
     if account is None:
@@ -128,8 +131,10 @@ def hub_schedule_routes(hubID):
 
     if request.method == 'GET':
         return get_hub_schedules(account, cursor, hubID)
+    elif request.method == 'POST':
+        return create_hub_schedule(account, cursor, connection, hubID)
 
-@hub_schedule.route('/hub/<hubID>/schedule/<scheduleID>', methods=['GET'])
+@hub_schedule.route('/hub/<hubID>/schedule/<scheduleID>', methods=['GET', 'DELETE', 'PATCH', 'POST'])
 def single_hub_schedule_routes(hubID, scheduleID):
     account = getAccount()
     if account is None:
@@ -144,3 +149,9 @@ def single_hub_schedule_routes(hubID, scheduleID):
 
     if request.method == 'GET':
         return get_one_hub_schedule(account, cursor, hubID, scheduleID)
+    elif request.method == 'DELETE':
+        return remove_hub_schedule(account, cursor, connection, hubID, scheduleID)
+    elif request.method == 'PATCH':
+        return update_hub_schedule(account, cursor, connection, hubID, scheduleID)
+    elif request.method == 'POST':
+        return add_hub_schedule(account, cursor, connection, hubID, scheduleID)
