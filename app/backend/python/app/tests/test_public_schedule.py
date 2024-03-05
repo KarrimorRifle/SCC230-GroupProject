fd5036e197b0f25425cef615c25bd43178b9c9f1
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-03-05 19:51:40 +0000

    Create get public schedule list test

--- app/backend/python/app/tests/test_public_schedule.py
+++ app/backend/python/app/tests/test_public_schedule.py
@@ -6,3 +6,17 @@ class TestPublicScheduleRoutes(unittest.TestCase):
     def setUp(self):
         self.client_server = app.test_client()
         self.client_server.post("/login", json={"Email": "jhondoe@gmail.com", "Password": "JhonDoe123."})
+
+    def test_get_public_schedules(self):
+        response = self.client_server.get("/schedule/public")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertIsInstance(data, list)
+        
+        for entry in data:
+            self.assertIn('ScheduleID', entry)
+            self.assertIn('ScheduleName', entry)
+            self.assertIn('IsActive', entry)
+            self.assertIn('IsDraft', entry)
+            self.assertIn('IsPublic', entry)
+            self.assertIn('Rating', entry)
