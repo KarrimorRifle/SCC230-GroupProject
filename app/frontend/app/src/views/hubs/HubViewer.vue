aa922fbc959e6964b7feb7d0fa375953d4a36fa4
Samuel To <samuelto888@yahoo.co.uk>
2024-03-11 02:44:39 +0000

    remove code

--- app/frontend/app/src/views/hubs/HubViewer.vue
+++ app/frontend/app/src/views/hubs/HubViewer.vue
@@ -10,67 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import axios from "axios";
-import { HubsList } from "@/modules/hubs/types";
-
-const hubList = ref<HubsList[]>([]);
-const searchValue = ref<string>("");
-
-//sorting menu
-const sortRank = ref<"NONE" | "ASC" | "DESC">("NONE");
-const sortCount = ref<"NONE" | "ASC" | "DESC">("NONE");
-
-const setup = async () => {
-  let data = await axios.get("http://localhost:5000/hub", {
-    withCredentials: true,
-  });
-
-  console.log(data.data);
-  hubList.value = data.data;
-};
-
-const filteredHubs = computed(() => {
-  let filter = hubList.value;
-
-  if (sortCount.value != "NONE")
-    filter.sort((hub1, hub2) =>
-      sortCount.value == "ASC"
-        ? hub1.UserCount - hub2.UserCount
-        : hub2.UserCount - hub1.UserCount
-    );
-
-  if (sortRank.value != "NONE")
-    filter.sort((hub1, hub2) =>
-      sortRank.value == "ASC"
-        ? hub1.PermissionLevel - hub2.PermissionLevel
-        : hub2.PermissionLevel - hub1.PermissionLevel
-    );
-
-  return filter.filter((hub) => hub.HubName.includes(searchValue.value));
-});
-
-const makeHub = async () => {
-  await axios.post(
-    "http://localhost:5000/hub",
-    {
-      HubName: `HUB ${hubList.value.length}`,
-    },
-    {
-      withCredentials: true,
-    }
-  );
-  setup();
-};
-
-const deleteHub = async (id: string) => {
-  await axios.delete(`http://localhost:5000/hub/${id}`, {
-    withCredentials: true,
-  });
-  setup();
-};
-
-setup();
+console.log("HI");
 </script>
 
 <style lang="scss">
