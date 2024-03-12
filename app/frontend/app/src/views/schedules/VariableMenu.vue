ebe3c01a26dc553bec0ce60a9dba6f44b53ce1d4
Samuel To <samuelto888@yahoo.co.uk>
2024-03-12 04:53:55 +0000

    add ability to add variables

--- app/frontend/app/src/views/schedules/VariableMenu.vue
+++ app/frontend/app/src/views/schedules/VariableMenu.vue
@@ -2,10 +2,15 @@
   <div class="container-fluid main-schedule-variable-manager px-0">
     <button @click="console.log(modelValue)">hello</button>
     <h3>Variables</h3>
-    <div class="variable-container">
+    <div v-if="Object.keys(modelValue).length > 0" class="variable-container">
       <div v-for="(value, key, index) in modelValue" :key="key + index">
         I am a variable
       </div>
+    </div>
+    <div v-else class="variable-container text-muted">
+      Uh oh! Nothings here...
+      <br />If this is unexpected please contact us on
+      <a href="tel:+447696696969">+44 7696 696969</a>
     </div>
     <div class="variable-controller">
       <div class="container-fluid">
@@ -17,8 +22,14 @@
                 class="form-control"
                 placeholder="VariableName: No special chars"
                 @input="removeSpecialCharacters"
+                v-model="newVarName"
               />
-              <button class="btn btn-success me-2 input-group-text">ADD</button>
+              <button
+                class="btn btn-success me-2 input-group-text"
+                @click="addNewVar"
+              >
+                ADD
+              </button>
             </div>
           </div>
           <div class="col-2">
@@ -33,8 +44,10 @@
 <script setup lang="ts">
 import { ref, defineProps, watchEffect } from "vue";
 
+const newVarName = ref<string>("");
+
 const props = defineProps<{
-  modelValue?: Record<string, "NUMBER" | "BOOLEAN">;
+  modelValue: Record<string, "NUMBER" | "BOOLEAN">;
 }>();
 
 let modelValue = ref(props.modelValue);
@@ -46,10 +59,15 @@ const removeSpecialCharacters = (event: InputEvent) => {
   const target = event.target as HTMLInputElement;
   target.value = target.value.replace(/[^a-zA-Z0-9]/g, "");
 };
+
+const addNewVar = () => {
+  modelValue.value[newVarName.value] = "NUMBER";
+  newVarName.value = "";
+};
 </script>
 <style>
 .main-schedule-variable-manager {
-  background: rgb(103, 103, 137);
+  background: rgb(85, 85, 113);
   height: 100%;
   min-height: 0;
   display: flex;
