dff1665488c3dd9d48b8955ff47017384d4ff87a
MrNudy <blade@eastie.net>
2024-03-14 08:58:30 +0000

    Fixed error 500 bug on Device update.

--- app/frontend/app/src/views/hubs/pages/DeviceList.vue
+++ app/frontend/app/src/views/hubs/pages/DeviceList.vue
@@ -344,10 +344,12 @@ const deleteDevice = (id: string) =>
     });
 
 const saveDevice = async () => {
+  delete editingDevice.value["Vars"];
+  console.log(editingDevice.value);
   let data = await axios
     .patch(
       `http://localhost:5000/hub/${HubID}/device/${editingDevice.value?.DeviceID}`,
-      editingDevice,
+      editingDevice.value,
       {
         withCredentials: true,
       }
